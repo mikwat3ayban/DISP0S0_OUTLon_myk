a6d844f9eb681d7e53e1c6540245f40372e2fd17
GH0STAV0 <you@example.com>
2022-08-07 02:52:43 +0100

    my connecto python

--- src/vendor/composer/autoload_real.php
+++ src/vendor/composer/autoload_real.php
@@ -13,19 +13,24 @@ class ComposerAutoloaderInitaa0b39385f5e64ad4c25b54d37853f67
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitaa0b39385f5e64ad4c25b54d37853f67', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitaa0b39385f5e64ad4c25b54d37853f67', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitaa0b39385f5e64ad4c25b54d37853f67::getInitializer($loader));
         } else {
@@ -60,11 +65,16 @@ class ComposerAutoloaderInitaa0b39385f5e64ad4c25b54d37853f67
     }
 }
 
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
 function composerRequireaa0b39385f5e64ad4c25b54d37853f67($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
         $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
